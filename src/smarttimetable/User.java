74bdad8b2c79df2402b44f95a5e2951c206beefc
Adam-PC <Adam-PC@ADAM-PC.home>
2017-08-12 14:44:32 +0100

    Changed editor fold description

--- src/smarttimetable/User.java
+++ src/smarttimetable/User.java
@@ -5,21 +5,25 @@ public class User {
     private static int userID;
     private static String username, password;
 
+    //Constructer never used as static doesn't need to be initialisd
     private User() {
     }
 
+    //Changes the variables to the variables provided
     static public void newUser(int userID, String username, String password){
         User.userID = userID;
         User.username = username;
         User.password = password;
     } 
     
+    //Changes the variables to null
     static public void logoutUser(){
         User.userID = 0;
         User.username = null;
         User.password = null;
     }
 
+    //<editor-fold defaultstate="collapsed" desc=" Getters ">
     static int getUserID() {
         return User.userID;
     }
@@ -31,5 +35,5 @@ public class User {
     static String getPassword() {
         return User.password;
     }
-
+    //</editor-fold>
 }
