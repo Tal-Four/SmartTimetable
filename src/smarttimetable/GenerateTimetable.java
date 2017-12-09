389420ba351ad074453004bb15cf990b7cfecc25
crazy <crazy@DESKTOP-NOEP8IP.home>
2017-12-09 17:05:49 +0000

    Finished event insertion into timetableSlot

--- src/smarttimetable/GenerateTimetable.java
+++ src/smarttimetable/GenerateTimetable.java
@@ -20,9 +20,20 @@ public class GenerateTimetable {
     private String getTodayDate() {
         GregorianCalendar calendar = new GregorianCalendar();
         int day = calendar.get(calendar.DATE);
+        String date;
+        if (day <= 9) {
+            date = "0" + day;
+        } else {
+            date = "" + day;
+        }
         int month = calendar.get(calendar.MONTH) + 1;
+        if (month <= 9) {
+            date = "0" + month + "-" + date;
+        } else {
+            date = month + "-" + date;
+        }
         int year = calendar.get(calendar.YEAR);
-        String date = year + "-" + month + "-" + day;
+        date = year + "-" + date;
         return date;
     }
 
@@ -33,9 +44,10 @@ public class GenerateTimetable {
         int inputDay = Integer.parseInt(date.substring(8, 10));
 
         GregorianCalendar calendar = new GregorianCalendar(inputYear, inputMonth, inputDay);
-        int day = calendar.get(calendar.DATE) - ((calendar.get(calendar.DAY_OF_WEEK) + 5) % 7);
-        int month = calendar.get(calendar.MONTH) + 1;
-        int year = calendar.get(calendar.YEAR);
+        calendar.add(GregorianCalendar.DAY_OF_YEAR, -((calendar.get(GregorianCalendar.DAY_OF_WEEK) + 5) % 7));
+        int day = calendar.get(GregorianCalendar.DATE);
+        int month = calendar.get(GregorianCalendar.MONTH) + 1;
+        int year = calendar.get(GregorianCalendar.YEAR);
 
         String monday = year + "-" + month + "-" + day;
         return monday;
@@ -54,7 +66,7 @@ public class GenerateTimetable {
         plotReccuringEvents(timetableID);
 
         plotSingleEvents(timetableID);
-        
+
     }
 
     private void plotReccuringEvents(int timetableID) {
@@ -85,32 +97,33 @@ public class GenerateTimetable {
     }
 
     private void plotSingleEvents(int timetableID) {
-        
+
         //Selects the start day of a timetable with the given ID
         String sql = "SELECT timetable.StartDay FROM user, timetable WHERE timetable.UserID = user.UserID "
                 + "AND user.UserID = " + User.getUserID() + " AND timetable.Hidden = 0 "
-                + "AND timetable.TimetableID = " + timetableID ;
+                + "AND timetable.TimetableID = " + timetableID;
         ResultSet rs = DatabaseHandle.query(sql);
-        
+
         //Creates a calendar that is set to the day the parsed timetable starts
         Date startDay = null;
         GregorianCalendar calendar = new GregorianCalendar();
         try {
+            rs.next();
             startDay = rs.getDate("StartDay");
         } catch (SQLException e) {
             System.err.println(e);
         }
         calendar.setTime(startDay);
         String date;
-        
+
         //Selects the events that have dates that fall within the week of the timetable starting
         for (int day = 0; day < 7; day++) {
             date = calendar.get(GregorianCalendar.YEAR) + "-"
-                    + calendar.get(GregorianCalendar.MONTH) + "-"
+                    + (1 + calendar.get(GregorianCalendar.MONTH)) + "-"
                     + calendar.get(GregorianCalendar.DATE);
-            sql = "SELECT event.* FROM event, user WHERE event.Day = NULL AND user.UserID = event.UserID "
-                    + "AND user.UserID = " + User.getUserID() + " AND event.Date = " + date
-                    + " ORDER BY event.StartTime";
+            sql = "SELECT event.* FROM event, user"
+                    + " WHERE user.UserID = event.UserID AND user.UserID = " + User.getUserID()
+                    + " AND event.Date = '" + date + "' ORDER BY event.StartTime";
             rs = DatabaseHandle.query(sql);
 
             try {
@@ -120,6 +133,8 @@ public class GenerateTimetable {
                     int endTime = (int) (rs.getFloat("EndTime") * 2);
                     int eventID = rs.getInt("EventID");
 
+                    //Inserts events into the timetableSlot table
+                    //For loop loops through all the slots required to fill the gap between start and finish
                     for (int slotCounter = startTime; slotCounter <= endTime; slotCounter++) {
                         sql = "INSERT INTO timetableslot (UserID, TimetableID, Day, Time, EventID) "
                                 + "VALUES (" + User.getUserID() + ", " + timetableID + ", " + day
